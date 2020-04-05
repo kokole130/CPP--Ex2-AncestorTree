02ce4d50c61142e0ac99583f2151b5c09a460b0c
Ron AZu <kokole130@gmail.com>
2020-04-05 20:44:28 +0300

    few changes

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -4,53 +4,30 @@
 #include <stdexcept>
 
 using namespace std;
-
-void FamilyTree::addNew(string name, char sex){ //add new related to the tree
-    if(start.name!=string("")&&(sex=='m'||sex=='f')){
-        start.name=name;
-        start.sex=sex;
-        return;
-    }
-    else{
-        return;
-        // we need to throw here Exception !!!
+using namespace family;
 
 
-    }
-}
-
-void FamilyTree::addNew(string name, char sex,char relation,string related){//add new related to the tree
-
-}
-
-string FamilyTree::findRelation(string name,char related){//find name of the related
-    return "";
-}
-// we need to fix it- cpp and hpp files.
-// we need to decide about findRelation function. 
-// name, or name+related.
-string FamilyTree::findRelation(string name){//find name of the related
+string Tree::relation(string name){//find name of the related
     return "";
 }
 
-string FamilyTree::find(string name1, string name2){//find the relation between to names
+string Tree::find(string relation){//find the relation between to names
     return "";
 }
 
-void FamilyTree::display(Node start){
+void Tree::display(){
 
 }
 
-void FamilyTree::remove(string name){
+void Tree::remove(string name){
 
 }
  
-
-void FamilyTree::addFather(string father,string name){
+Tree& Tree::addFather(string name,string father){
 
 }
 
-void FamilyTree::addMother(string mother,string name){
+Tree& Tree::addMother(string name,string mother){
 
 }
 
--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -1,44 +1,33 @@
 #pragma once
 #include <string>
-#include <vector>
 #include <iostream>
 
 using namespace std;
-
+namespace family{
 typedef struct Node{//each node in the tree
         string name;//name of the person
-        char sex;
+        char gender;
         struct Node* F;//one node for father
         struct Node* M;//one node for mother
-        struct Node* W;//one node for wife
-        vector<struct Node> S;//dynamic list for sibling
-        vector<struct Node> C;//dynamic list for cousin
-        vector<struct Node> U;//dynamic list for uncle
-        vector<struct Node> A;//dynamic list for aunt
-        vector<struct Node> G;//dynamic list for grand parent
-
 }Node;
 
-class FamilyTree {
+class Tree {
 
     public:
-        Node start;
-        FamilyTree(){
-            start.name="";
+        Node root;
+        Tree(string name){
+            root.name=name;
         }
 
-        void addNew(string name, char sex);
-        void addNew(string name, char sex,char relation,string related);
-        string findRelation(string name,char related);
-        string findRelation(string name); // we need to fix it. 
-        string find(string name1, string name2);
-        void display(Node start);
+        string relation(string name); // we need to fix it. 
+        string find(string relation);
+        void display();
         void remove(string name);
-        void addFather(string father,string name);
-        void addMother(string mother,string name);
+        Tree& addFather(string name,string father);
+        Tree& addMother(string name,string mother);
 
 };
-
+}
 
 
 
