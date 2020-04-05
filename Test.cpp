0a1a2fd878c384c22078bbf5e91fe2d68c8659e6
omriyonatani <omriyonatani@gmail.com>
2020-04-05 21:35:38 +0300

    Tests

--- Test.cpp
+++ Test.cpp
@@ -5,32 +5,16 @@ using namespace std;
 using namespace family;
 
 
-// Functions to make global trees - T1,T2,T3.
+// Functions to make global tree
 Tree& makeTree(){
-    Tree T1("yosef");
-	T1.addFather("Yosef", "Yaakov")   // Tells the tree that the father of Yosef is Yaakov.
+    Tree T("Yosef");
+	T.addFather("Yosef", "Yaakov")   // Tells the tree that the father of Yosef is Yaakov.
 	 .addMother("Yosef", "Rachel")   // Tells the tree that the mother of Yosef is Rachel.
 	 .addFather("Yaakov", "Isaac")
 	 .addMother("Yaakov", "Rivka")
 	 .addFather("Isaac", "Avraham")
 	 .addFather("Avraham", "Terah");
-    return T1;
-}
-
-Tree& makeTree2(){
-    Tree T2("Ron");
-    T2.addFather("ronius");
-    T2.addMother("roniousa");
-    T2.addFather("dalar");
-    return T2;
-}
-
-Tree& makeTree3(){
-    Tree T3("Yogev");
-    T3.addMother("dana");
-    T3.addFather("dan");
-
-    return T3;
+    return T;
 }
 
 
@@ -38,131 +22,50 @@ Tree& makeTree3(){
 
 TEST_CASE("Unrelated Case"){
     Tree T = makeTree();
-    CHECK(T.relation("zz", 's') == string("unrelated"));
-	CHECK(T.relation("gg", 'c') == string("unrelated"));
-	CHECK(T.relation("kk", 'w') == string("unrelated"));
-	CHECK(T.relation("vvf", 'g') == string("unrelated"));
-	CHECK(T.relation("ddf", 'f') == string("unrelated"));
-	CHECK(T.relation("tth", 'm') == string("unrelated"));
-	CHECK(T.relation("uuk", 'u') == string("unrelated"));
-	CHECK(T.relation("qq", 'a') == string("unrelated"));
-	CHECK(T.relation("qqe", 'c') == string("unrelated"));
-	CHECK(T.relation("rqwdd", 'c') == string("unrelated"));
+    CHECK(T.relation("Omri") == string("unrelated"));
+	CHECK(T.relation("Dan") == string("unrelated"));
+	CHECK(T.relation("Shmuel") == string("unrelated"));
+	CHECK(T.relation("Yaakov") == string("father"));
+	CHECK(T.relation("Rachel") == string("mother"));
+	CHECK(T.relation("sava") == string("unrelated"));
+	CHECK(T.relation("Isaac") == string("grandfather"));
+	CHECK(T.relation("Quar") == string("unrelated"));
+	CHECK(T.relation("quar") == string("unrelated"));
+	CHECK(T.relation("Rivka") == string("grandmother"));
 }
 
-TEST_CASE("Father/Mother find Case"){
-    FamilyTree T = makeTree();
-    T.addFather("adam","shmuel");
-    T.addMother("adam","Dana");
-    CHECK(T.find("adam","father") == string("shmuel"));
-    CHECK(T.find("adam","mother") == string("Dana"));
-    T.addFather("adam","shmueli");
-    T.addMother("adam","Danit");
-    CHECK(T.find("adam","father") == string("shmuel"));
-    CHECK(T.find("adam","mother") == string("Dana"));
-    T.addFather("adam","dan");
-    T.addMother("adam","ziva");
-    CHECK(T.find("adam","father") == string("shmuel"));
-    CHECK(T.find("adam","mother") == string("Dana"));
-    T.addFather("adam","ron");
-    T.addMother("adam","rona");
-    CHECK(T.find("adam","father") == string("shmuel"));
-    CHECK(T.find("adam","mother") == string("Dana"));
-    T.addFather("adam","kfir");
-    T.addMother("adam","rivka");
-    CHECK(T.find("adam","father") == string("shmuel"));
-    CHECK(T.find("adam","mother") == string("Dana"));
+TEST_CASE("find Case"){
+    Tree T = makeTree();
+    CHECK(T.find("grandmother") == string("Rivka"));
+    CHECK(T.find("grandfather") == string("Isaac"));
+    CHECK(T.find("Mother") == string("Rachel"));
+    T.remove("Terah");
+    T.addFather("Avraham", "Omer");
+    CHECK(T.find("great-great-grandfather") == string("Omer"));
+    T.addMother("Isaac","Dana");
+    CHECK(T.find("great-grandmother") == string("Dana"));
+    T.addMother("Omer","Olga");
+    CHECK(T.find("Olga") == string("great-great-great-great-grandmother"));
+    T.remove("Yaakov");
+    CHECK_THROWS(T.find("dana"));
+    CHECK_THROWS(T.find("avi"));
+    CHECK_THROWS(T.find("avichai"));
+    CHECK_THROWS(T.find("avihai"));
 }
 
 TEST_CASE("Exception Case") {
-    FamilyTree T2 = makeTree2();
-    CHECK_THROWS(T2.findRelation("ron"));
-    CHECK_THROWS(T2.findRelation("omri"));
-    CHECK_THROWS(T2.findRelation("yogev"));
-    CHECK_THROWS(T2.findRelation("dag"));
-    CHECK_THROWS(T2.findRelation("sos"));
-    CHECK_THROWS(T2.findRelation("hamor"));
-    CHECK_THROWS(T2.findRelation("ori"));
-    CHECK_THROWS(T2.findRelation("shani"));
-    CHECK_THROWS(T2.findRelation("dana"));
-    CHECK_THROWS(T2.findRelation("avi"));
-    CHECK_THROWS(T2.findRelation("avichai"));
-    CHECK_THROWS(T2.findRelation("avihai"));
-
-    FamilyTree T = makeTree();
-    T.remove("adam"); // remove the whole tree.
-    CHECK_THROWS(T2.findRelation("avi"));
-    CHECK_THROWS(T2.findRelation("avichai"));
-    CHECK_THROWS(T2.findRelation("avihai"));
-    CHECK_THROWS(T2.find("adam","mother"));
-    CHECK_THROWS(T2.find("adam","cousin"));
-    CHECK_THROWS(T2.find("adam","father"));
-}
-
-TEST_CASE("remove function") {
     Tree T = makeTree();
-    CHECK(T.find("yaakov") == string("Dana"));
-
-}
-
-TEST_CASE("Find relation with relation assign"){
-    FamilyTree T = makeTree();
-    T.addFather("yaakov","adam");
-    T.addMother("zipi","adam");
-    T.addMother("mira","hava");
-    T.addFather("yagai","hava");
-    T.addNew("haim",'m','s',"yaakov");
-    T.addNew("juli",'f','c',"mira");
-    T.addNew("koral",'f','w',"yefet");
-    T.addNew("moshe",'m','g',"posher");
-    T.addNew("menash",'m','g',"shem");
-    T.addNew("tikva",'f','c',"mira");
-    CHECK(T.findRelation("hava")==string("wife"));
-    CHECK(T.findRelation("ham")==string("wife/cousin"));
-    CHECK(T.findRelation("posher")==string("wife/cousin/cousin"));
-    CHECK(T.findRelation("mira")==string("wife/mother"));
-    CHECK(T.findRelation("yaakov")==string("father"));
-    CHECK(T.findRelation("juli")==string("wife/mother/cousin"));
-    CHECK(T.findRelation("shem")==string("cousin"));
-    T.addNew("roni",'f','w',"shem");
-    T.addNew("ben",'m','s',"roni");
-    T.addNew("lior",'m','g',"shem");
-    T.addNew("miri",'f','w',"ben");
-    CHECK(T.findRelation("ben")==string("cousin/wife/sibling"));
-    CHECK(T.findRelation("menash")==string("cousin/grand parent"));
-    CHECK(T.findRelation("miri")==string("cousin/wife/sibling/wife"));
-    CHECK(T.findRelation("kar")==string("wife/cousin/wife"));
-    CHECK_THROWS(T.findRelation("bar"));
-    CHECK_THROWS(T.findRelation("rivka"));
-
-}
-
-TEST_CASE("finds relation between 2 nodes in the tree"){
-    FamilyTree T = makeTree();
-    T.addFather("yaakov","adam");
-    T.addMother("zipi","adam");
-    T.addMother("mira","hava");
-    T.addFather("yagai","hava");
-    T.addNew("haim",'m','s',"yaakov");
-    T.addNew("juli",'f','c',"mira");
-    T.addNew("koral",'f','w',"yefet");
-    T.addNew("moshe",'m','g',"posher");
-    T.addNew("menash",'m','g',"shem");
-    T.addNew("tikva",'f','c',"mira");
-    T.addNew("roni",'f','w',"shem");
-    T.addNew("ben",'m','s',"roni");
-    T.addNew("lior",'m','g',"shem");
-    T.addNew("miri",'f','w',"ben");
-    CHECK(T.find("shem","hava")==string("cousin/wife"));
-    CHECK(T.find("yaakov","mira")==string("father/wife/mother"));
-    CHECK(T.find("ham","posher")==string("cousin"));
-    CHECK(T.find("kar","adam")==string("wife/cousin/wife"));
-    CHECK(T.find("yefet","zipi")==string("sibling/cousin/wife/mother"));
-    CHECK(T.find("koral","posher")==string("wife/sibling/cousin"));
-    CHECK(T.find("roni","miri")==string("sibling/wife"));
-    CHECK(T.find("miri","ham")==string("wife/sibling/wife/cousin/wife/cousin"));
-    CHECK(T.find("moshe","yagai")==string("grand parent/cousin/cousin/father"));
-    CHECK_THROWS(T.find("shem","yael"));
-    CHECK_THROWS(T.find("yam","baroch"));
-    CHECK_THROWS(T.find("zlil","hava"));
+    T.remove("Yosef"); // remove the whole tree.
+    CHECK_THROWS(T.find("ron"));
+    CHECK_THROWS(T.find("omri"));
+    CHECK_THROWS(T.find("yogev"));
+    CHECK_THROWS(T.find("dag"));
+    CHECK_THROWS(T.find("sos"));
+    CHECK_THROWS(T.find("hamor"));
+    CHECK_THROWS(T.find("ori"));
+    CHECK_THROWS(T.find("shani"));
+    CHECK_THROWS(T.find("dana"));
+    CHECK_THROWS(T.find("avi"));
+    CHECK_THROWS(T.find("avichai"));
+    CHECK_THROWS(T.find("avihai"));
 }
